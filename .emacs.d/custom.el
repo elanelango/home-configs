1a5f10714cad8ed57f18521f8543d303303a61b8
Elanchezhiyan Elango <elanelango@gmail.com>
2018-06-10 16:53:55 -0700

    remove customization I don't remember about

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,47 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
     ("190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
  '(evil-want-C-u-scroll t)
- '(fci-rule-color "#383838")
  '(menu-bar-mode nil)
- '(mu4e-change-filenames-when-moving t)
- '(mu4e-view-prefer-html t)
- '(mu4e-view-show-images t)
  '(package-selected-packages
    (quote
     (magit key-chord powerline evil zenburn-theme highlight-indentation elscreen multi-term yaml-mode flycheck yasnippet auto-complete)))
  '(tool-bar-mode nil)
- '(tramp-terminal-type "dumb")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+ '(tramp-terminal-type "dumb"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
